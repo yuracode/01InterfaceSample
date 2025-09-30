415f7e838a196627d491f2ae2661a8b232bb6d1f
Hiroyuki Sasaki <hry-sasaki@yoshida-g.ac.jp>
2025-09-30 19:28:19 +0900

    project name changed

--- src/use/Call.java
+++ src/use/Call.java
@@ -1,15 +1,18 @@
 package use;
 
+import used.Calculator;
+import used.SubCalc;
+
 /**
 * インターフェース依存を確認するためのクラス
 */
 public class Call {
 	public static void main(String[] args) {
 		// 減算クラスをインスタンス化
-//		Calculator calculator = new SubCalc();
+		Calculator calculator = new SubCalc();
 		// メソッドを実行
-//		Integer result = calculator.calc(10, 5);
+		Integer result = calculator.calc(10, 5);
 		// 結果の表示
-//		System.out.println("計算結果は「" + result + "」です。");
+		System.out.println("計算結果は「" + result + "」です。");
 	}
 }
